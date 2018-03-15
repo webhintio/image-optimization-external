568b3e925ae972faf6c26dda10a96c33f6e9cb53
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2018-03-15 16:28:00 -0700

    Fix: Update `sonarwhal` to `v1.0.5`

--- src/rules/optimize-image/optimize-image.ts
+++ src/rules/optimize-image/optimize-image.ts
@@ -4,8 +4,9 @@
 
 import { Category } from 'sonarwhal/dist/src/lib/enums/category';
 import { RuleContext } from 'sonarwhal/dist/src/lib/rule-context';
-import { IRule, IRuleBuilder, IScanEnd, ITargetFetchStart } from 'sonarwhal/dist/src/lib/types';
+import { IRule, ScanEnd, FetchEnd, RuleMetadata } from 'sonarwhal/dist/src/lib/types';
 import { debug as d } from 'sonarwhal/dist/src/lib/utils/debug';
+import { RuleScope } from 'sonarwhal/dist/src/lib/enums/rulescope';
 
 import { Result, File, Bytes } from './types';
 
@@ -17,106 +18,108 @@ const debug: debug.IDebugger = d(__filename);
  * ------------------------------------------------------------------------------
  */
 
-const rule: IRuleBuilder = {
-    create(context: RuleContext): IRule {
-        /** Error processing the request. */
-        let failed: boolean = false;
-        /** The promise that represents the analyzing result. */
-        let analyzePromise: Promise<Result>;
+export default class OptimizeImageRule implements IRule {
 
-        const username: string = process.env.SERVICE_USERNAME; // eslint-disable-line no-process-env
-        const endpoint: string = process.env.API_ENDPOINT; // eslint-disable-line no-process-env
-        const requestAsync = require('request-promise');
-
-        const notifyError = async (resource: string, error: any) => {
-            debug(`Error getting analyzing result for ${resource}.`, error);
-
-            await context.report(resource, null, `Couldn't get results for ${resource}. Error: ${error.message}`);
-        };
-
-        const validateTargetFetchStart = (targetFetchStart: ITargetFetchStart) => {
-            const { resource }: { resource: string } = targetFetchStart;
-
-            debug(`Validating rule.`);
-
-            if (!username) {
-                notifyError(resource, new Error('No username is provided for authentication.'));
-                failed = true;
-
-                return;
-            }
-
-            const options = {
-                auth: { user: username },
-                body: { url: resource },
-                json: true,
-                method: 'POST',
-                uri: endpoint
-            };
-
-            analyzePromise = requestAsync(options);
-            analyzePromise.catch(async (error) => {
-                failed = true;
-
-                await notifyError(resource, error);
-            });
-        };
-
-        const valiateScanEnd = async (fetchEnd: IScanEnd) => {
-            const { resource }: { resource: string } = fetchEnd;
-
-            if (!analyzePromise || failed) {
-                return;
-            }
-
-            debug(`Waiting for the optimizing result of ${resource}.`);
-
-            const result: Result = await analyzePromise;
-
-            debug(`Received the optimizing result of ${resource}.`);
-
-            const { files }: { files: Array<File> } = result;
-            const unoptimized: Array<File> = files.filter((file) => {
-                if (!file) {
-                    return false;
-                }
-
-                const { bytes }: { bytes: Bytes } = file;
-
-                return bytes.output < bytes.input;
-            });
-
-            if (!unoptimized.length) {
-                debug(`All images are optimized.`);
-
-                return;
-            }
-
-            const reportPromises: Array<Promise<void>> = unoptimized.map((file) => {
-                const { bytes }: { bytes: Bytes } = file;
-                const sizeDiff: number = bytes.savings / 1024;
-                const percentageDiff: number = Math.round((bytes.savings / bytes.input) * 100);
-
-                return context.report(file.url, null, `File "${file.name}" can be ${sizeDiff.toFixed(2)}kB (${percentageDiff}%) smaller.`);
-            });
-
-            await Promise.all(reportPromises);
-        };
-
-        return {
-            'scan::end': valiateScanEnd,
-            'targetfetch::start': validateTargetFetchStart
-        };
-    },
-    meta: {
+    public static readonly meta: RuleMetadata = {
         docs: {
             category: Category.performance,
             description: `Optimize images.`
         },
-        recommended: false,
+        id: 'optimize-image',
         schema: [],
-        worksWithLocalFiles: false
+        scope: RuleScope.site
     }
-};
 
-module.exports = rule;
+    /** Error processing the request. */
+    private failed: boolean = false;
+    /** The promise that represents the analyzing result. */
+    private analyzePromise: Promise<Result>;
+
+    private username: string = process.env.SERVICE_USERNAME; // eslint-disable-line no-process-env
+    private endpoint: string = process.env.API_ENDPOINT; // eslint-disable-line no-process-env
+
+    private context: RuleContext;
+
+    private async notifyError(resource: string, error: any) {
+        debug(`Error getting analyzing result for ${resource}.`, error);
+
+        await this.context.report(resource, null, `Couldn't get results for ${resource}. Error: ${error.message}`);
+    }
+
+    private validateTargetFetchStart(targetFetchStart: FetchEnd) {
+        const requestAsync = require('request-promise');
+        const { resource }: { resource: string } = targetFetchStart;
+
+        debug(`Validating rule.`);
+
+        if (!this.username) {
+            this.notifyError(resource, new Error('No username is provided for authentication.'));
+            this.failed = true;
+
+            return;
+        }
+
+        const options = {
+            auth: { user: this.username },
+            body: { url: resource },
+            json: true,
+            method: 'POST',
+            uri: this.endpoint
+        };
+
+        this.analyzePromise = requestAsync(options);
+        this.analyzePromise.catch(async (error) => {
+            this.failed = true;
+
+            await this.notifyError(resource, error);
+        });
+    }
+
+    private async valiateScanEnd(fetchEnd: ScanEnd) {
+        const { resource }: { resource: string } = fetchEnd;
+
+        if (!this.analyzePromise || this.failed) {
+            return;
+        }
+
+        debug(`Waiting for the optimizing result of ${resource}.`);
+
+        const result: Result = await this.analyzePromise;
+
+        debug(`Received the optimizing result of ${resource}.`);
+
+        const { files }: { files: Array<File> } = result;
+        const unoptimized: Array<File> = files.filter((file) => {
+            if (!file) {
+                return false;
+            }
+
+            const { bytes }: { bytes: Bytes } = file;
+
+            return bytes.output < bytes.input;
+        });
+
+        if (!unoptimized.length) {
+            debug(`All images are optimized.`);
+
+            return;
+        }
+
+        const reportPromises: Array<Promise<void>> = unoptimized.map((file) => {
+            const { bytes }: { bytes: Bytes } = file;
+            const sizeDiff: number = bytes.savings / 1024;
+            const percentageDiff: number = Math.round((bytes.savings / bytes.input) * 100);
+
+            return this.context.report(file.url, null, `File "${file.name}" can be ${sizeDiff.toFixed(2)}kB (${percentageDiff}%) smaller.`);
+        });
+
+        await Promise.all(reportPromises);
+    }
+
+    public constructor(context: RuleContext) {
+        this.context = context;
+
+        context.on('scan::end', this.valiateScanEnd.bind(this));
+        context.on('fetch::end::html', this.validateTargetFetchStart.bind(this));
+    }
+}
